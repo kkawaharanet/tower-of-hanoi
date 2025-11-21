f4094502abe572289ff285b1310c81c840fad0bb
kkawaharanet <kkawaharanet@gmail.com>
2025-11-21 17:14:20 +0900

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { version } from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/software/tower-of-hanoi",
+  base: "/development/tower-of-hanoi",
   define: {
     "import.meta.env.VERSION": JSON.stringify(version),
   },
